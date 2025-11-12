988d3e9e04ab8a8fe6dcb987443c5da96366f458
tanvuong0609 <tanvuong060905@gmail.com>
2025-11-12 15:47:00 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
+import LandingPage from './pages/LandingPage.jsx'
 
 function App() {
   return (
